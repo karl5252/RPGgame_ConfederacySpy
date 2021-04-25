ad64646a7af92352c69998ddadc7d1f51ed15b03
Karol <karl.michalik@gmail.com>
2021-04-25 15:04:05 +0200

    Modified UI; added characer class properties; changed Character to public

---
+++ Engine/Models/GameSession.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.Models
+{
+    class GameSession
+    {
+        Character currentPlayer { get; set; }
+        GameSession()
+        {
+            currentPlayer = new Character();
+            currentPlayer.Name = "Han Solo";
+            currentPlayer.CharacterClass = "Clawdithe";
+            currentPlayer.HitPoints = 10;
+            currentPlayer.ExperiencePoints = 0;
+            currentPlayer.Gold = 10;
+        }
+    }
+}
