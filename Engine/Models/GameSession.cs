c512ddca720ed5e22ea835771624e4dc5703a196
Karol <karl.michalik@gmail.com>
2021-04-25 23:05:47 +0200

    Added location
    To add WORLD
    to add MAP
    to add excel parser

--- Engine/Models/GameSession.cs
+++ Engine/Models/GameSession.cs
@@ -10,6 +10,8 @@ namespace Engine.Models
     public class GameSession
     {
         public Character currentPlayer { get; set; }
+        public Location currentLocation { get; set; }
+
         public GameSession()
         {
             currentPlayer = new Character();
@@ -18,6 +20,13 @@ namespace Engine.Models
             currentPlayer.HitPoints = 10;
             currentPlayer.ExperiencePoints = 0;
             currentPlayer.Gold = 10;
+
+            currentLocation = new Location();
+            currentLocation.Name = "Hideout";
+            currentLocation.XCoordinate = 0;
+            currentLocation.YCoordinate = -1;
+            currentLocation.Description = "At least it is not drawing attention...";
+            currentLocation.ImageName = "/Engine;component/Images/Locations/hideout.jpg";
         }
     }
 }
